90ef6f3aee40439910879bdac99b302ce2e74755
Keagan Collins <keaganncollins@gmail.com>
2025-02-01 12:44:40 +0200

    use okular as default pdf viewer for latex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,6 +2,6 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	init = function()
-		-- vim.g.vimtex_view_method = 'start "C:/Program Files/Okular/bin/okular.exe"'
+		-- vim.g.vimtex_view_method = 'okular'
 	end
 }
